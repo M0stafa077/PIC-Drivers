5bce7d4bc3c7df012d9cd2df3544312a0af62455
Mostafa_Asaad <ma4190243@gmail.com>
2024-03-19 23:42:19 +0200

    SPI configuration file

---
+++ MCAL/SPI/spi_cfg.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   spi_cfg.h
+ * Author: Mostafa Asaad
+ *
+ * Created on February 24, 2024, 3:05 PM
+ */
+
+#ifndef SPI_CFG_H
+#define	SPI_CFG_H
+
+/* --------------- Section: Macro Declarations --------------- */
+#define SPI_MULTISLAVE          STD_FALSE
+#define SPI_NUMBER_OF_SLAVES    0X01
+
+
+#endif	/* SPI_CFG_H */
+
