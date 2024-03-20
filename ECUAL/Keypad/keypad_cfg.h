6390fba974b0394c90295d073014f80a83051aa3
Mostafa_Asaad <ma4190243@gmail.com>
2024-03-20 12:51:35 +0200

    Keypad configuration file

---
+++ ECUAL/Keypad/keypad_cfg.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   keypad_cfg.h
+ * Author: Mostafa_Asaad
+ *
+ * Created on December 8, 2023, 11:38 PM
+ */
+
+#ifndef KEYPAD_CFG_H
+#define	KEYPAD_CFG_H
+
+/* Section : Includes */
+
+/* Section: Macro Declarations */
+
+/* Section: Macro Functions Declarations */
+
+/* Section: Data Type Declarations */
+
+/* Section: Function Declarations */
+
+#endif	/* KEYPAD_CFG_H */
+
